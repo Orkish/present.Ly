5985fc2a03bf94a500c8680a4e4c1c8cb5659ad2
Winson Yuan <winsonyuan1@gmail.com>
2014-09-09 21:56:48 -0400

    student json obj created, ctrl created

--- script/script.js
+++ script/script.js
@@ -1,3 +1,13 @@
 (function(){
 	var app = angular.module('present.Ly', []);
+
+	app.controler("EmailCtrl", function($scope, $http){
+		$http.get("script/FEWD.json").
+			success(function(data){
+				$scope.FEWD = data;
+			}).
+			error(function(data){
+				console.log("something's wrong");
+			});
+	});
 })();
